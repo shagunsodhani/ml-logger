a54d201f98cc4a4b85ec8038205fcab98446795d
Shagun Sodhani <sodhani@fb.com>
2019-11-14 09:30:02 -0800

    Add first version of logbook

---
+++ ml_logger/logbook.py
@@ -0,0 +1,69 @@
+import json
+from typing import Dict
+import time
+
+from ml_logger import filesystem_logger as fs_log
+from ml_logger.utils import flatten_dict, make_dir
+
+
+class LogBook:
+    """Logging utility for ML Experiments"""
+
+    def __init__(self, logbook_config: Dict) -> LogBook:
+        self._process_rank = logbook_config["process_rank"]
+        fs_log.set_logger(logger_file_path=logbook_config["logger_file_path"])
+        # self.tensorboard_writer = None
+        # self.should_use_tb = config.logger.tensorboard.should_use
+        # if self.should_use_tb:
+        #     self.tensorboard_writer = SummaryWriter(comment=config.logger.project_name)
+
+    def log_metrics_to_remote(self, kwargs: Dict) -> None:
+        """Method to log the metrics to remote backend"""
+        pass
+
+    def preprocess_log(self, log: Dict) -> Dict:
+        """"Method to preprocess the log before logging"""
+        log["process_rank"] = self._process_rank
+        log["timestamp"] = time.strftime("%I:%M%p %Z %b %d, %Y")
+        return log
+
+    def write_config_log(self, config: Dict) -> None:
+        """Write config"""
+        processed_config = self.preprocess_log(config)
+        fs_log.write_config_log(processed_config)
+
+    def write_metric_logs(self, metrics: Dict) -> None:
+        """Write Metric to the filesystem"""
+        processed_metric = self.preprocess_log(metric)
+        fs_log.write_metric_logs(processed_metric)
+        # if self.should_use_tb:
+
+        #     timestep_key = "num_timesteps"
+        #     for key in set(list(metrics.keys())) - set([timestep_key]):
+        #         self.tensorboard_writer.add_scalar(
+        #             tag=key,
+        #             scalar_value=metrics[key],
+        #             global_step=metrics[timestep_key],
+        #         )
+
+    def write_compute_logs(self, metrics: Dict) -> None:
+        """Write Compute Logs"""
+        return self.write_metric_logs(metrics=metrics)
+
+    def write_message_logs(self, message) -> None:
+        """Write message logs"""
+        processed_message = self.preprocess_log(message)
+        fs_log.write_message_logs(processed_message)
+
+    def write_metadata_logs(self, metadata: Dict) -> None:
+        """Write metadata"""
+        processed_metadata = self.preprocess_log(metadata)
+        fs_log.write_metadata_logs(processed_metadata)
+
+
+def make_config(logger_file_path: str, process_rank: str = "0") -> Dict:
+    """Method to prepare the config dict that will be passed to
+    the Logbook constructor.
+    `process_rank` flag is useful when using multi-processing"""
+    config = {"process_rank": process_rank, "logger_file_path": logger_file_path}
+    return config
