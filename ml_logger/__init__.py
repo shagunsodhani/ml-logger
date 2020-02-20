d764cd534877ebab93b87715986a4d87b387f67b
Shagun Sodhani <sodhani@fb.com>
2020-02-20 12:58:05 -0800

    Setup __init__.py

---
+++ ml_logger/__init__.py
@@ -0,0 +1,13 @@
+from ml_logger import filesystem_logger, logbook, metrics, parser, types, wandb
+
+__version__ = "0.2"
+
+__all__ = [
+    "__version__",
+    "filesystem_logger",
+    "logbook",
+    "metrics",
+    "parser",
+    "types",
+    "wandb",
+]
