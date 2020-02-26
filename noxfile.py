012cb84dd0026a6ab09eb87bc41c3d5e20d48c12
Shagun Sodhani <sodhani@fb.com>
2020-02-25 20:22:42 -0800

    Fix noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,9 +2,10 @@ import nox
 
 
 @nox.session(venv_backend="conda")
-def lint(session):
+def lint(session) -> None:
     session.install("--upgrade", "setuptools", "pip")
-    session.install("-r", "requirements.txt")
+    session.install("-r", "requirements/all.txt")
+    session.install("-r", "requirements/dev.txt")
     session.run("flake8", "ml_logger")
     session.run("black", "--check", "ml_logger")
     session.run("mypy", "--strict", "ml_logger")
