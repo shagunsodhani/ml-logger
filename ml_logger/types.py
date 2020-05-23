39afe1998a75b97d75aac6b6c68a999a1615c61a
Shagun Sodhani <sodhani@fb.com>
2020-05-23 03:48:02 -0700

    Add ParseLineFunctionType

--- ml_logger/types.py
+++ ml_logger/types.py
@@ -1,8 +1,9 @@
-from typing import Any, Callable, Dict, Union
+from typing import Any, Callable, Dict, Optional, Union
 
 NumType = Union[int, float]
 ValueType = Union[str, int, float]
 LogType = Dict[str, Any]
+ParseLineFunctionType = Callable[[str], Optional[LogType]]
 ConfigType = LogType
 MetricType = Dict[str, ValueType]
 ModelType = Any
