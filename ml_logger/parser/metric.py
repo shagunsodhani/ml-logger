906aba36656c824645488a69e73f969c90bc5e19
Shagun Sodhani <sodhani@fb.com>
2020-05-23 03:46:49 -0700

    Config and Metric parsers are inherited from base parsers

--- ml_logger/parser/metric.py
+++ ml_logger/parser/metric.py
@@ -1,17 +1,26 @@
-"""Implementation of Parser to parse the logs"""
+"""Implementation of Parser to parse metrics from logs"""
 
-import json
 from typing import Callable, Dict, List, Optional
 
 import pandas as pd
 
-from ml_logger.parser import parser as base_parser
+from ml_logger.parser import log as log_parser
 from ml_logger.parser import utils as parser_utils
-from ml_logger.types import LogType, MetricType
+from ml_logger.parser.utils import parse_json
+from ml_logger.types import LogType, MetricType, ParseLineFunctionType
 
 
-def fn_to_group_metrics(metrics: List[MetricType]) -> Dict[str, List[MetricType]]:
-    """Function to group a list of metrics into a dictionary of (key,
+def parse_json_and_match_key(line: str) -> Optional[LogType]:
+    log = parse_json(line)
+    if log:
+        key = "logbook_type"
+        if key not in log or log[key] != "metric":
+            log = None
+    return log
+
+
+def group_metrics(metrics: List[MetricType]) -> Dict[str, List[MetricType]]:
+    """Group a list of metrics into a dictionary of (key,
     list of grouped metrics)
 
     Args:
@@ -24,8 +33,8 @@ def fn_to_group_metrics(metrics: List[MetricType]) -> Dict[str, List[MetricType]
     return {"all": metrics}
 
 
-def fn_to_aggregate_metrics(metrics: List[MetricType]) -> List[MetricType]:
-    """Function to aggregate a list of metrics
+def aggregate_metrics(metrics: List[MetricType]) -> List[MetricType]:
+    """Aggregate a list of metrics
 
     Args:
         metrics (List[MetricType]): List of metrics to aggregate
@@ -36,112 +45,89 @@ def fn_to_aggregate_metrics(metrics: List[MetricType]) -> List[MetricType]:
     return metrics
 
 
-class Parser(base_parser.Parser):
-    """Class to parse the metrics in the log files
-    """
+class Parser(log_parser.Parser):
+    """Class to parse the metrics from the logs"""
 
-    def __init__(
-        self,
-        log_transformer: Callable[
-            [LogType], LogType
-        ] = parser_utils.identity_log_transformer,
-        error_handler: Callable[
-            [str, json.decoder.JSONDecodeError], Optional[LogType]
-        ] = parser_utils.silent_error_handler,
-    ):
-        """Class to parse the log files
+    def __init__(self, parse_line: ParseLineFunctionType = parse_json_and_match_key):
+        """Class to parse the metrics from the logs
 
         Args:
-            log_transformer (Callable[[LogType], LogType], optional):
-                Function to transform the logs after reading them from the
-                filesystem. Defaults to parser_utils.identity_log_transformer.
-            error_handler (Callable[[str, json.decoder.JSONDecodeError],
-                Optional[LogType]], optional): Function to handle the
-                error when the parser reads an invalid json string.
-                Defaults to parser_utils.silent_error_handler.
+            parse_line (ParseLineFunctionType):
+                Function to parse a line in the log file. The function
+                should return None if the line is not a valid log statement
+                (eg error messages). Defaults to parse_json_and_match_key.
         """
-
-        super().__init__(
-            log_transformer=log_transformer, error_handler=error_handler,
-        )
+        super().__init__(parse_line)
         self.log_type = "metric"
 
     def get_metrics_as_df(
         self,
-        file_path: str,
-        fn_to_group_metrics: Callable[
+        filepath_pattern: str,
+        group_metrics: Callable[
             [List[LogType]], Dict[str, List[LogType]]
-        ] = fn_to_group_metrics,
-        fn_to_aggregate_metrics: Callable[
-            [List[LogType]], List[LogType]
-        ] = fn_to_aggregate_metrics,
+        ] = group_metrics,
+        aggregate_metrics: Callable[[List[LogType]], List[LogType]] = aggregate_metrics,
     ) -> Dict[str, pd.DataFrame]:
         """Create a dict of (metric_name, dataframe)
 
         Method that:
-        (i) reads the requested metrics (for each specified mode),
+        (i) reads metrics from the filesystem
         (ii) groups metrics
-        (iii) merge all the metrics in a group,
-        (iv) converts the merged metrics into dataframes and returns a list of dataframes
+        (iii) aggregates all the metrics within a group,
+        (iv) converts the aggregate metrics into dataframes and returns a
+            dictionary of dataframes
 
         Args:
-            file_path (str): Log file to read from
-            fn_to_group_metrics (Callable[[List[LogType]], Dict[str, List[LogType]]], optional):
+            filepath_pattern (str): filepath pattern to glob
+            group_metrics (Callable[[List[LogType]], Dict[str, List[LogType]]], optional):
                 Function to group a list of metrics into a dictionary of
-                (key, list of grouped metrics). Defaults to fn_to_group_metrics.
-            fn_to_aggregate_metrics (Callable[[List[LogType]], List[LogType]], optional):
-                Function to aggregate a list of metrics. Defaults to fn_to_aggregate_metrics.
-
-        Returns:
-            Dict[str, pd.DataFrame]: [description]
+                (key, list of grouped metrics). Defaults to group_metrics.
+            aggregate_metrics (Callable[[List[LogType]], List[LogType]], optional):
+                Function to aggregate a list of metrics. Defaults to aggregate_metrics.
 
         """
-        metric_logs = list(self.get_logs(file_path=file_path))
-        return self.metrics_to_df(
+        metric_logs = list(self.parse(filepath_pattern))
+        return metrics_to_df(
             metric_logs=metric_logs,
-            fn_to_group_metrics=fn_to_group_metrics,
-            fn_to_aggregate_metrics=fn_to_aggregate_metrics,
+            group_metrics=group_metrics,
+            aggregate_metrics=aggregate_metrics,
         )
 
-    def metrics_to_df(
-        self,
-        metric_logs: List[LogType],
-        fn_to_group_metrics: Callable[
-            [List[LogType]], Dict[str, List[LogType]]
-        ] = fn_to_group_metrics,
-        fn_to_aggregate_metrics: Callable[
-            [List[LogType]], List[LogType]
-        ] = fn_to_aggregate_metrics,
-    ) -> Dict[str, pd.DataFrame]:
-        """Create a dict of (metric_name, dataframe)
 
-        Method that:
-        (i) groups metrics
-        (ii) merge all the metrics in a group,
-        (iii) converts the merged metrics into dataframes and returns a list of dataframes
+def metrics_to_df(
+    metric_logs: List[LogType],
+    group_metrics: Callable[[List[LogType]], Dict[str, List[LogType]]] = group_metrics,
+    aggregate_metrics: Callable[[List[LogType]], List[LogType]] = aggregate_metrics,
+) -> Dict[str, pd.DataFrame]:
+    """Create a dict of (metric_name, dataframe)
 
-        Args:
-            metric_logs (List[LogType]): List of metrics
-            fn_to_group_metrics (Callable[[List[LogType]], Dict[str, List[LogType]]], optional):
-                Function to group a list of metrics into a dictionary of
-                (key, list of grouped metrics). Defaults to fn_to_group_metrics.
-            fn_to_aggregate_metrics (Callable[[List[LogType]], List[LogType]], optional):
-                Function to aggregate a list of metrics. Defaults to fn_to_aggregate_metrics.
+    Method that:
+    (i) groups metrics
+    (ii) aggregates all the metrics within a group,
+    (iii) converts the aggregate metrics into dataframes and returns a
+        dictionary of dataframes
 
-        Returns:
-            Dict[str, pd.DataFrame]: [description]
+    Args:
+        metric_logs (List[LogType]): List of metrics
+        group_metrics (Callable[[List[LogType]], Dict[str, List[LogType]]], optional):
+            Function to group a list of metrics into a dictionary of
+            (key, list of grouped metrics). Defaults to group_metrics.
+        aggregate_metrics (Callable[[List[LogType]], List[LogType]], optional):
+            Function to aggregate a list of metrics. Defaults to aggregate_metrics.
 
-        """
-        grouped_metrics: Dict[str, List[LogType]] = fn_to_group_metrics(metric_logs)
-        merged_metrics = {
-            key: fn_to_aggregate_metrics(metrics)
-            for key, metrics in grouped_metrics.items()
-        }
+    Returns:
+        Dict[str, pd.DataFrame]: [description]
 
-        metric_dfs = {
-            key: pd.DataFrame.from_dict(
-                data=parser_utils.map_list_of_dicts_to_dict_of_lists(metrics)
-            )
-            for key, metrics in merged_metrics.items()
-        }
-        return metric_dfs
+    """
+    grouped_metrics: Dict[str, List[LogType]] = group_metrics(metric_logs)
+    aggregated_metrics = {
+        key: aggregate_metrics(metrics) for key, metrics in grouped_metrics.items()
+    }
+
+    metric_dfs = {
+        key: pd.DataFrame.from_dict(
+            data=parser_utils.map_list_of_dicts_to_dict_of_lists(metrics)
+        )
+        for key, metrics in aggregated_metrics.items()
+    }
+    return metric_dfs
