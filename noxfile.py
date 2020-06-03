ae2073c3291ddb0214dcebdde4e7dda7e904c84f
Shagun Sodhani <sodhani@fb.com>
2020-06-03 04:58:45 -0700

    Add separate sessions for mypy and lint

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,25 @@
 # type: ignore
 import nox
+from nox.sessions import Session
 
 
 @nox.session()
-def lint(session) -> None:
+def lint(session: Session) -> None:
     session.install("--upgrade", "setuptools", "pip")
-    session.install("-r", "requirements/filesystem.txt")
-    session.install("-r", "requirements/all.txt")
     session.install("-r", "requirements/dev.txt")
     session.run("flake8", "ml_logger")
     session.run("black", "--check", "ml_logger")
+
+
+@nox.session()
+def mypy(session: Session) -> None:
+    session.install("--upgrade", "setuptools", "pip")
+    session.install("-r", "requirements/dev.txt")
     session.run("mypy", "--strict", "ml_logger")
 
 
 @nox.session()
 def test_metrics(session) -> None:
     session.install("--upgrade", "setuptools", "pip")
-    session.install("-r", "requirements/filesystem.txt")
-    session.install("-r", "requirements/all.txt")
     session.install("-r", "requirements/dev.txt")
     session.run("pytest", "tests")
