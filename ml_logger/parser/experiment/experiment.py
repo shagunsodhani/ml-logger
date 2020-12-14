a6df4c332fa3e94d74c6d658878249c0df0e9562
Shagun Sodhani <sodhani@fb.com>
2020-12-14 04:40:53 -0800

    Add method to aggregate an experiment sequence

--- ml_logger/parser/experiment/experiment.py
+++ ml_logger/parser/experiment/experiment.py
@@ -8,7 +8,7 @@ from typing import Any, Callable, Dict, List, Optional
 
 import pandas as pd
 from ml_logger import utils
-from ml_logger.types import ConfigType, MetricType
+from ml_logger.types import ConfigType
 
 ExperimentMetricType = Dict[str, pd.DataFrame]
 ExperimentInfoType = Dict[Any, Any]
@@ -111,6 +111,53 @@ def deserialize(dir_path: str) -> Experiment:
     return Experiment(configs=configs, metrics=metrics, info=info)
 
 
+def return_first_config(config_lists: List[List[ConfigType]]) -> List[ConfigType]:
+    """Return the first config list, from a list of list of configs. Otherwise
+    return empty list.
+
+    Args:
+        config_lists (List[List[ConfigType]])
+
+    Returns:
+        List[ConfigType]
+    """
+    for config_list in config_lists:
+        if len(config_list) > 0:
+            return config_list
+    return []
+
+
+def concat_metrics(metric_list: List[ExperimentMetricType]) -> ExperimentMetricType:
+    """Concatenate the metrics.
+
+    Args:
+        metric_list (List[ExperimentMetricType])
+
+    Returns:
+        ExperimentMetricType
+    """
+    concatenated_metrics = {}
+    metric_keys = metric_list[0].keys()
+    for key in metric_keys:
+        concatenated_metrics[key] = pd.concat([metric[key] for metric in metric_list])
+    return concatenated_metrics
+
+
+def return_first_infos(info_list: List[ExperimentInfoType]) -> ExperimentInfoType:
+    """Return the first info, from a list of infos. Otherwise return empty info.
+
+    Args:
+        info_list (List[ExperimentInfoType])
+
+    Returns:
+        ExperimentInfoType
+    """
+    for info in info_list:
+        if info is not None:
+            return info
+    return {}
+
+
 class ExperimentSequence(UserList):  # type: ignore
     def __init__(self, experiments: List[Experiment]):
         """List-like interface to a collection of Experiments."""
@@ -151,4 +198,22 @@ class ExperimentSequence(UserList):  # type: ignore
         """
         return ExperimentSequence(
             [experiment for experiment in self.data if filter_fn(experiment)]
+        )
+
+    def aggregate(
+        self,
+        aggregate_configs: Callable[
+            [List[List[ConfigType]]], List[ConfigType]
+        ] = return_first_config,
+        aggregate_metrics: Callable[
+            [List[ExperimentMetricType]], ExperimentMetricType
+        ] = concat_metrics,
+        aggregate_infos: Callable[
+            [List[ExperimentInfoType]], ExperimentInfoType
+        ] = return_first_infos,
+    ) -> Experiment:
+        return Experiment(
+            configs=aggregate_configs(*[exp.config for exp in self.data]),
+            metrics=aggregate_metrics(*[exp.metrics for exp in self.data]),
+            infos=aggregate_infos(*[exp.infos for exp in self.data]),
         )
